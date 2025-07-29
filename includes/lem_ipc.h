9ab9e6e40a9fac70da7304609a753f7fea421ff0
Loupthevenin <loup.thevenin.1@gmail.com>
2025-07-29 09:06:07 +0200

    start lemipc

---
+++ includes/lem_ipc.h
@@ -0,0 +1,7 @@
+#ifndef LEM_IPC_H
+# define LEM_IPC_H
+
+# include "../libft/ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,9 @@
+#include "../includes/lem_ipc.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	ft_printf("Hello world!");
+	return (0);
+}
